681869c7a6024448677e6083b16cc4256418c87b
SCAU-WH <153712002+SCAU-WH@users.noreply.github.com>
2023-12-26 15:43:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,18 +8,14 @@ if __name__ == '__main__':
     device = "cuda"
 
     root = ""
-    save_path = "paper_test/"
-    image_names = ["img_", "img_single_"]
-    img_len = 73
-    roi_len = 1
+    save_path = " "
 
     mobile_sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
     mobile_sam.to(device=device)
     mobile_sam.eval()
     predictor = SamPredictor(mobile_sam)
 
-    rangeROIs.main(predictor, root, save_path, "img_single_", img_len, roi_len)
+    rangeROIs.main(predictor, root, save_path, "", img_len, roi_len)
 
-    print('PyCharm')
 
 
